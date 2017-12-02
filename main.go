f137a4900b01da3eab71eb174faa596bdea53459
harindaka <harindaka@gmail.com>
2017-12-02 08:24:05 +0530

    added example derived from https://github.com/zserge/webview/blob/master/examples/counter-go/

--- main.go
+++ main.go
@@ -2,8 +2,42 @@ package main
 
 import "github.com/zserge/webview"
 
+// func main() {
+// 	// Open wikipedia in a 800x600 resizable window
+// 	webview.Open("Minimal webview example",
+// 		"https://en.m.wikipedia.org/wiki/Main_Page", 800, 600, true)
+// }
+
+// Counter is a simple example of automatic Go-to-JS data binding
+type Counter struct {
+	Value int `json:"value"`
+}
+
+// Add increases the value of a counter by n
+func (c *Counter) Add(n int) {
+	c.Value = c.Value + int(n)
+}
+
+// Reset sets the value of a counter back to zero
+func (c *Counter) Reset() {
+	c.Value = 0
+}
+
 func main() {
-	// Open wikipedia in a 800x600 resizable window
-	webview.Open("Minimal webview example",
-		"https://en.m.wikipedia.org/wiki/Main_Page", 800, 600, true)
+	w := webview.New(webview.Settings{
+		Title: "Click counter: " + uiFrameworkName,
+	})
+	defer w.Exit()
+
+	w.Dispatch(func() {
+		// Inject controller
+		w.Bind("counter", &Counter{})
+
+		// Inject CSS
+		w.InjectCSS(string(MustAsset("js/styles.css")))
+
+		// Inject web UI framework and app UI code
+		loadUIFramework(w)
+	})
+	w.Run()
 }
